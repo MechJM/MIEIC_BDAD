5fd81198f1e1eebb8a271bd898ebb9a242425d1e
MechJM <matosjoaops@protonmail.com>
2020-03-28 22:03:06 +0000

    Added more restrictions

--- Trabalho/criar.sql
+++ Trabalho/criar.sql
@@ -1,15 +1,15 @@
-PRAGMA foreign_keys=ON
+PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS Cliente;
-DROP TABLE IF EXISTS Funcionário;
+DROP TABLE IF EXISTS Funcionario;
 DROP TABLE IF EXISTS Enfermeiro;
-DROP TABLE IF EXISTS Médico;
-DROP TABLE IF EXISTS Horário;
+DROP TABLE IF EXISTS Medico;
+DROP TABLE IF EXISTS Horario;
 DROP TABLE IF EXISTS Equipamento;
-DROP TABLE IF EXISTS Serviço;
+DROP TABLE IF EXISTS Servico;
 DROP TABLE IF EXISTS Consulta;
 DROP TABLE IF EXISTS Cirurgia;
-DROP TABLE IF EXISTS Análise;
+DROP TABLE IF EXISTS Analise;
 DROP TABLE IF EXISTS Departamento;
 DROP TABLE IF EXISTS Realizam;
 DROP TABLE IF EXISTS Auxilia;
@@ -17,102 +17,112 @@ DROP TABLE IF EXISTS Usado_Em;
 
 CREATE TABLE Cliente(
     Nome VARCHAR(50), 
-    NIF INTEGER primary key not null,
+    NIF INTEGER primary key not null check (NIF > 0),
     Morada VARCHAR(300), 
-    Telefone INTEGER,
-    idCliente INTEGER unique, 
-    Historial_Médico VARCHAR(500)
+    Telefone INTEGER check (Telefone > 0),
+    idCliente INTEGER unique not null check (idCliente > 0), 
+    Historial_Medico VARCHAR(500)
 );
-CREATE TABLE Funcionário(
+CREATE TABLE Funcionario(
     Nome VARCHAR(50), 
-    NIF INTEGER primary key not null, 
+    NIF INTEGER primary key not null check (NIF > 0), 
     Morada VARCHAR(300), 
-    Telefone INTEGER,
-    idFuncionário INTEGER unique, 
-    Departamento VARCHAR(20)
+    Telefone INTEGER check (Telefone > 0),
+    idFuncionario INTEGER unique not null check (idFuncionario > 0), 
+    Departamento VARCHAR(20) references Departamento(nome) not null,
+    unique (NIF,Departamento)
 );
 CREATE TABLE Enfermeiro(
     Nome VARCHAR(50), 
-    NIF INTEGER primary key not null, 
+    NIF INTEGER primary key not null check (NIF > 0), 
     Morada VARCHAR(300), 
-    Telefone INTEGER,
-    idFuncionário INTEGER unique, 
-    Departamento VARCHAR(20),
-    Injeções_Administradas VARCHAR(50)
+    Telefone INTEGER check (Telefone > 0),
+    idFuncionario INTEGER unique not null check (idFuncionario > 0), 
+    Departamento VARCHAR(20) references Departamento(nome) not null,
+    Injecoes_Administradas VARCHAR(50),
+    unique (NIF,Departamento)
 );
-CREATE TABLE Médico(
+CREATE TABLE Medico(
     Nome VARCHAR(50), 
-    NIF INTEGER primary key not null, 
+    NIF INTEGER primary key not null check (NIF > 0), 
     Morada VARCHAR(300), 
-    Telefone INTEGER,
-    idFuncionário INTEGER unique, 
-    Departamento VARCHAR(20),
-    Disponibilidade_Plantão BLOB
+    Telefone INTEGER check (Telefone > 0),
+    idFuncionario INTEGER unique not null check (idFuncionario > 0), 
+    Departamento VARCHAR(20) references Departamento(nome) not null,
+    Especialidade VARCHAR(20) check (Especialidade = Departamento),
+    Disponibilidade_Plantao BLOB,
+    unique (NIF,Departamento)
 );
-CREATE TABLE Horário(
-    idHorário INTEGER primary key not null, 
-    idFuncionário INTEGER unique, 
+CREATE TABLE Horario(
+    idHorario INTEGER primary key not null check (idHorario > 0), 
+    idFuncionario INTEGER references Funcionario(idFuncionario) not null, 
     Dias_folga VARCHAR(100), 
     Dias_trabalho VARCHAR(100), 
     Turnos VARCHAR(30)
 );
 CREATE TABLE Equipamento(
-    Código INTEGER primary key not null, 
+    Codigo INTEGER primary key not null check (Codigo > 0), 
     Tipo VARCHAR(20), 
-    Quantidade INTEGER
+    Quantidade INTEGER check (Quantidade >= 1)
 );
-CREATE TABLE Serviço(
-    idServiço INTEGER primary key not null, 
+CREATE TABLE Servico(
+    idServico INTEGER primary key not null check (idServico > 0), 
     Data CHAR(10), 
     Hora CHAR(5), 
-    Custo FLOAT, 
-    idCliente INTEGER
+    Custo FLOAT check (Custo > 0), 
+    idCliente INTEGER references Cliente(idCliente) not null check (idCliente > 0),
+    unique (Data,Hora,idCliente)
 );
 CREATE TABLE Consulta(
-    idServiço INTEGER primary key not null, 
+    idServico INTEGER primary key not null check (idServico > 0), 
     Data CHAR(10), 
     Hora CHAR(5), 
-    Custo FLOAT, 
-    idCliente INTEGER, 
-    Diagnóstico VARCHAR(100), 
+    Custo FLOAT check (Custo > 0), 
+    idCliente INTEGER references Cliente(idCliente) not null check (idCliente > 0), 
+    Diagnostico VARCHAR(100), 
     Sintomas VARCHAR(50), 
-    Médico VARCHAR 
+    Medico VARCHAR references Medico(idFuncionario) not null,
+    unique (Data,Hora,idCliente),
+    unique (Data,Hora,Medico)
 );
 CREATE TABLE Cirurgia(
-    idServiço INTEGER primary key not null, 
+    idServico INTEGER primary key not null check (idServico > 0), 
     Data CHAR(10), 
     Hora CHAR(5), 
-    Custo FLOAT, 
-    idCliente INTEGER,
+    Custo FLOAT check (Custo > 0), 
+    idCliente INTEGER references Cliente(idCliente) not null check (idCliente > 0),
     Tipo_Procedimento VARCHAR(30), 
-    Órgão VARCHAR(10)
+    Orgao VARCHAR(10),
+    unique (Data,Hora,idCliente)
 );
-CREATE TABLE Análise(
-    idServiço INTEGER primary key not null, 
+CREATE TABLE Analise(
+    idServico INTEGER primary key not null check (idServico > 0), 
     Data CHAR(10), 
     Hora CHAR(5), 
-    Custo FLOAT, 
-    idCliente INTEGER,
+    Custo FLOAT check (Custo > 0), 
+    idCliente INTEGER references Cliente(idCliente) not null check (idCliente > 0),
     Nome VARCHAR(15), 
     Resultados VARCHAR(100), 
     Data_Resultado VARCHAR(10), 
-    idFuncionário INTEGER,
-    Consulta INTEGER 
+    idFuncionario INTEGER references Funcionario(idFuncionario) not null check (idFuncionario > 0),
+    Consulta INTEGER references Consulta(idServico) not null check (Consulta > 0),
+    unique (Data,Hora,idCliente),
+    unique (Data,Hora,idFuncionario)
 );
 CREATE TABLE Departamento(
     Nome VARCHAR(20) primary key not null,
-    Piso INTEGER
+    Piso INTEGER check (Piso >= -2 and Piso <= 5)
 );
 CREATE TABLE Realizam(
-    Médico INTEGER not null, 
-    Cirurgia INTEGER not null 
+    Medico INTEGER references Medico(idFuncionario) not null check (Medico > 0), 
+    Cirurgia INTEGER references Cirurgia(idServico) not null check (Cirurgia > 0)
 );
 CREATE TABLE Auxilia(
-    Enfermeiro INTEGER not null,
-    Cirurgia INTEGER not null
+    Enfermeiro INTEGER references Enfermeiro(idFuncionario) not null check (Enfermeiro > 0),
+    Cirurgia INTEGER references Cirurgia(idServico) not null check (Cirurgia > 0)
 );
 CREATE TABLE Usado_Em(
-    Código INTEGER not null,
-    idServiço INTEGER not null
+    Codigo INTEGER references Equipamento(Codigo) not null check (Codigo > 0),
+    idServico INTEGER references Servico(idServico) not null check (idServico > 0)
 );
 COMMIT;
