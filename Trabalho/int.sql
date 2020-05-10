b6b477e4f73d3929546378911c9164bf411768bf
MechJM <matosjoaops@protonmail.com>
2020-05-10 15:06:29 +0100

    Added another query

--- Trabalho/int.sql
+++ Trabalho/int.sql
@@ -6,4 +6,7 @@ select Nome,Injecoes_Administradas from Enfermeiro where Injecoes_Administradas 
 select '' as '';
 select idServico,Custo from Cirurgia where Custo > 40;
 select '' as '';
-select Nome,sum(Servico.Custo) from Cliente,Servico where Cliente.idCliente = Servico.idCliente group by Servico.idCliente;
+select Nome,sum(Servico.Custo) as Total from Cliente,Servico where Cliente.idCliente = Servico.idCliente group by Servico.idCliente order by Total;
+select '' as '';
+select Departamento.Nome,count(*) as NumFuncs from Funcionario,Departamento where Funcionario.Departamento = Departamento.Nome group by Funcionario.Departamento order by NumFuncs;
+select '' as '';
