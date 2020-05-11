aad6843cab34633a610ff2e22d311386ad9fccbd
MechJM <matosjoaops@protonmail.com>
2020-05-11 14:49:45 +0100

    Added more queries

--- Trabalho/int.sql
+++ Trabalho/int.sql
@@ -12,7 +12,8 @@ select Departamento.Nome,count(*) as NumFuncs from Funcionario,Departamento wher
 select '' as '';
 select distinct Cliente.Nome as Nome_do_Cliente, Medico.Especialidade as Especialidade from Cliente,Consulta,Medico where Cliente.idCliente = Consulta.idCliente and Consulta.Medico = Medico.idFuncionario order by Nome_do_Cliente;--As especialidades dos médicos de consultas de cada cliente
 select '' as '';
-select Nome from Cliente where idCliente in (select Cliente.idCliente from Cliente,Cirurgia where Cliente.idCliente = Cirurgia.idCliente and Cirurgia.Tipo_Procedimento like '%Transplante%');--nomes de clientes que efetuaram transplante
+select Nome from Cliente where idCliente in (select Cliente.idCliente from Cliente,Cirurgia where Cliente.idCliente = Cirurgia.idCliente and Cirurgia.Tipo_Procedimento like '%Transplante%');--nomes de clientes que efetuaram transplante, provavelmente não precisa da subquery
 select '' as '';
 select Medico.Nome as Nome,Medico.idFuncionario as idMedico,count(*) as numCirurgias from Medico,Cirurgia,Realizam where Realizam.Medico = Medico.idFuncionario and Realizam.Cirurgia = Cirurgia.idServico group by Medico.idFuncionario;
 select '' as '';
+select Cliente.Nome as Nome_do_Cliente from Cliente,Analise where Cliente.idCliente = Analise.idCliente and Analise.Nome like '%Ecocardiograma%' and Cliente.idCliente in (select Cliente.idCliente from Cliente,Consulta where Cliente.idCliente = Consulta.idCliente and Consulta.Diagnostico like '%Hipertensão%');--dos clientes que estiveram numa consulta onde foram diagnosticados com hipertensão, quais realizaram um ecocardiograma 
