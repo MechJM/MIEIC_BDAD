1f9bfd97ae94de8a449d3b772c201c14e71f170a
MechJM <matosjoaops@protonmail.com>
2020-05-12 19:23:27 +0100

    Added descriptions to queries

--- Trabalho/int.sql
+++ Trabalho/int.sql
@@ -2,19 +2,19 @@
 .headers on
 .nullvalue NULL
 
-select Nome,Injecoes_Administradas from Enfermeiro where Injecoes_Administradas > 100;
+select Nome,Injecoes_Administradas from Enfermeiro where Injecoes_Administradas > 100;--Enfermeiros com mais de 100 injeções administradas
 select '' as '';
-select idServico,Custo from Cirurgia where Custo > 40;
+select idServico,Custo from Cirurgia where Custo > 40;--Serviços com custo superior a 40 euros
 select '' as '';
-select Nome,sum(Servico.Custo) as Total from Cliente,Servico where Cliente.idCliente = Servico.idCliente group by Servico.idCliente order by Total;
+select Nome,sum(Servico.Custo) as Total from Cliente,Servico where Cliente.idCliente = Servico.idCliente group by Servico.idCliente order by Total;--Total gasto por cada cliente
 select '' as '';
-select Departamento.Nome,count(*) as NumFuncs from Funcionario,Departamento where Funcionario.Departamento = Departamento.Nome group by Funcionario.Departamento order by NumFuncs;
+select Departamento.Nome,count(*) as NumFuncs from Funcionario,Departamento where Funcionario.Departamento = Departamento.Nome group by Funcionario.Departamento order by NumFuncs;--Número de funcionários por departamento
 select '' as '';
 select distinct Cliente.Nome as Nome_do_Cliente, Medico.Especialidade as Especialidade from Cliente,Consulta,Medico where Cliente.idCliente = Consulta.idCliente and Consulta.Medico = Medico.idFuncionario order by Nome_do_Cliente;--As especialidades dos médicos de consultas de cada cliente
 select '' as '';
 select Nome from Cliente where idCliente in (select Cliente.idCliente from Cliente,Cirurgia where Cliente.idCliente = Cirurgia.idCliente and Cirurgia.Tipo_Procedimento like '%Transplante%');--nomes de clientes que efetuaram transplante, provavelmente não precisa da subquery
 select '' as '';
-select Medico.Nome as Nome,Medico.idFuncionario as idMedico,count(*) as numCirurgias from Medico,Cirurgia,Realizam where Realizam.Medico = Medico.idFuncionario and Realizam.Cirurgia = Cirurgia.idServico group by Medico.idFuncionario;
+select Medico.Nome as Nome,Medico.idFuncionario as idMedico,count(*) as numCirurgias from Medico,Cirurgia,Realizam where Realizam.Medico = Medico.idFuncionario and Realizam.Cirurgia = Cirurgia.idServico group by Medico.idFuncionario;--número de cirurgias realizadas por cada médico
 select '' as '';
 select Cliente.Nome as Nome_do_Cliente from Cliente,Analise where Cliente.idCliente = Analise.idCliente and Analise.Nome like '%Ecocardiograma%' and Cliente.idCliente in (select Cliente.idCliente from Cliente,Consulta where Cliente.idCliente = Consulta.idCliente and Consulta.Diagnostico like '%Hipertensão%');--dos clientes que estiveram numa consulta onde foram diagnosticados com hipertensão, quais realizaram um ecocardiograma 
 select '' as '';
