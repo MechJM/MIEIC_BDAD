dfbc3736b26100e2936c66e60d363940d0b3e4a5
MechJM <matosjoaops@protonmail.com>
2020-05-23 15:56:30 +0100

    Corrected another mistake

--- Trabalho/gatilho4_verifica.sql
+++ Trabalho/gatilho4_verifica.sql
@@ -2,6 +2,6 @@ PRAGMA foreign_keys=ON;
 
 select * from Analise;
 
-insert into Analise VALUES(100,'2020-04-20','16:00',25,7,'TAC','Tumor','2020-04-27',4,9);
+insert into Analise VALUES(100,'2020-04-20','16:00',25,4,'TAC','Tumor','2020-04-27',4,9);
 
 select * from Analise;
