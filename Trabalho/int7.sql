6b65133c9fc0727ac60f05189e5a4f5299434e5f
MechJM <matosjoaops@protonmail.com>
2020-05-23 15:50:27 +0100

    Updated query no.7

--- Trabalho/int7.sql
+++ Trabalho/int7.sql
@@ -2,4 +2,4 @@
 .headers	on
 .nullvalue	NULL
 
-SELECT count(*) FROM Funcionario WHERE idFuncionario = (SELECT idFuncionario FROM Horario WHERE turnoNoturno = 1);--Mostrar o numero de funcionarios que trabalham à noite
+SELECT count(*) FROM Funcionario WHERE idFuncionario in (SELECT idFuncionario FROM Horario WHERE turnoNoturno = 1);--Mostrar o numero de funcionarios que trabalham à noite
