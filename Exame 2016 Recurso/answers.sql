b3bd5609f0cbfd14de15b2f2f520416401fcc8a4
MechJM <matosjoaops@protonmail.com>
2020-06-22 23:26:20 +0100

    Last solutions for this exam

--- Exame 2016 Recurso/answers.sql
+++ Exame 2016 Recurso/answers.sql
@@ -6,5 +6,23 @@ select Estudante.nome as Nome from Estudante where Estudante.id in (select ID1 f
 
 drop TABLE if exists AmizadeTransitiva;
 create table AmizadeTransitiva (ID1 int REFERENCES Estudante(id), ID2 int REFERENCES Estudante(id), PRIMARY KEY(ID1,ID2));
-insert into AmizadeTransitiva  select distinct Am1.ID1,Am2.ID2 from Amizade as Am1, Amizade as Am2 where Am1.ID2 = Am2.ID1 and Am1.ID1 != Am2.ID2 and (Am1.ID1,Am2.ID2) not in (select * from Amizade) order by Am1.ID1,Am2.ID2;
---insert into AmizadeTransitiva  SELECT ID2,ID1 from AmizadeTransitiva;
+insert into AmizadeTransitiva select distinct Am1.ID1,Am2.ID2 from Amizade as Am1, Amizade as Am2 where Am1.ID2 = Am2.ID1 and Am1.ID1 != Am2.ID2 and (Am1.ID1,Am2.ID2) not in (select * from Amizade) order by Am1.ID1,Am2.ID2;
+
+select '' as '';
+select E1.nome as Nome,E2.nome as Nome from Amizade, Estudante as E1, Estudante as E2 where Amizade.ID1 = E1.id and Amizade.ID2 = E2.id and E1.curso != E2.curso and E1.id < E2.id;
+select '' as '';
+
+create trigger if not exists amRem after delete on Amizade 
+begin
+delete from Amizade where ID1 = OLD.ID2 and ID2 = OLD.ID1;
+end;
+
+create trigger if not exists amIns after insert on Amizade 
+begin
+insert into Amizade values (NEW.ID2,NEW.ID1);
+end;
+
+create trigger if not exists amUpd before update on Amizade
+begin
+select raise(rollback,"Can't update friendship");
+end;
